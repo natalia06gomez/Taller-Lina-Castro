302680f495c6311ec005335a00fa4a53f9f92512
linan <linan@LAPTOP-LV1J7A9G>
2024-02-15 15:57:44 -0500

    Taller

---
+++ src/co/edu/unipiloto/estDatos/generics/date/Datable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.edu.unipiloto.estDatos.generics.date;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface Datable {
+    
+    public int month();
+    public int day();
+    public int year();
+    
+}
