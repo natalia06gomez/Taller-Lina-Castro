a4ee83157ef671d19fca8da5cec4f31bc3e81443
linan <linan@LAPTOP-LV1J7A9G>
2024-02-15 15:57:53 -0500

    Taller

---
+++ src/co/edu/unipiloto/estDatos/generics/date/ComparableDate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.edu.unipiloto.estDatos.generics.date;
+
+import java.util.Date;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface ComparableDate {
+    public int compareTo (Date d);
+    
+}
